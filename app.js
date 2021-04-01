8038f5fbb36cea75ee5f7a87e4f6e4c9870fd630
Varfolomeev-Ilya <ilya323472@yandex.ru>
2021-04-01 16:43:21 +0300

    created new schemas with dependencies for Tables and tasks

--- app.js
+++ app.js
@@ -4,19 +4,34 @@ const { errors } = require('celebrate');
 const app = express();
 const userRouter = require('./routes/userRouters');
 const accountRouter = require('./routes/accountRouters');
+const boardRouter = require('./routes/boardsRouters')
+const multer = require('multer');
 const cors = require('cors');
 const corsOptions = {
   origin: "http://localhost:3000"
 };
 app.use(cors(corsOptions))
-// app.set("view engine", "hbs");
-// app.use("/account", userRouter, (req,res) => {
+// app.use("/account", (req,res) => {
 //   res.send(req.file)
 // });
+const upload = multer({dest:"avatars"});
+// app.use(express.static(__dirname));
+// app.post("/account", upload.single("filedata"),
+//  function (req, res, next) {
+   
+//   let filedata = req.file;
+
+//   console.log(filedata);
+//   if(!filedata)
+//       res.send("Ошибка при загрузке файла");
+//   else
+//       res.send("Файл загружен");
+// });
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", userRouter, accountRouter);
+app.use( "/", userRouter, accountRouter, boardRouter );
 app.use((req, res) => {
   res.status(404).send("Not found");
 });
