6cef7d73e238052d9ca794f9d01cf888c5fab728
Varfolomeev-Ilya <ilya323472@yandex.ru>
2021-04-19 17:54:38 +0300

    admins controller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,26 +1,29 @@
 require('dotenv').config();
 const models = require('../db/models');
-const { response } = require('express');
 require('../middleware/loadingAvatar');
 
 exports.getAllUsers = async (req, res, next) => {
   try {
-    // const admin = await models.User.findOne({
-    //   where: {
-    //     roleId: req.query.roleId
-    //   },
-    // });
+    const { roleId, id } = req.query;
+    if (req.query.roleId == 2) {
+      const user = await models.User.findOne({
+        where: {
+          id: id,
+        },
+      });
+    }
 
-    // if (!admin) {
-    //   throw new Error('user is not a admin');
-    // };
+    if (!user) {
+      throw new Error('user no found');
+    };
 
     const allUsers = await models.User.findAll({
       order: [['id', 'ASC']],
+      // attributes: ['id', 'firstName', 'lastName', 'email'],
     });
     return res.status(200).json({ message: 'All users', allUsers });
   } catch (err) {
-    next(error);
+    return res.status(404).json({ message: err.message });
   };
 };
 
@@ -42,11 +45,27 @@ exports.updateOneUser = async (req, res, next) => {
         returning: true,
         plain: true,
       }
-     );
+    );
     const user = updatedUser[1].dataValues;
     delete user.password;
     return res.status(200).json(user);
   } catch (error) {
     next(error);
+  };
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const deletedUser = await models.User.destroy({
+      where: { id },
+    });
+    const allusers = await models.User.findAll();
+    if (!deletedUser) {
+      throw new Error('User not found');
+    }
+    return res.status(202).json(allusers);
+  } catch (err) {
+    return res.status(404).json({ message: err.message });
   };
 };
